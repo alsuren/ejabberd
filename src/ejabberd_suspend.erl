d8813de7d536395b07fcff8b87936a4a95d744f1
David Laban <alsuren@gmail.com>
2013-03-01 13:56:22 +0000

    Re-structure to become an FSM
    * Cribbing heavily from ejabberd_c2s.erl and web/ejabberd_http_bind.erl
    
    * The opaque SockData (#sockdata) struct is what's passed across the boundary
      between ejabberd_c2s and ejabberd_suspend. Do not confuse this with
      StateData (#suspend_state), which is the FSM state.
    
    * Make handle_sync_event handle {send, Packet} and {send_xml, Packet}.
     * Note that send_xml is probably a lie.
    
    The process I used went a bit like this:
    until make | forbid Warning:
    do
        seek to first error in ejabberd_suspend.erl
        fix any obvious errors
        copy-paste from ejabberd_c2s.erl or ejabberd_http_bind.erl
    done
    
    git commit -am "IT BUILDS: SHIP IT!"
    
    until make && deploy && make -C ~/src/ejabberd_tests check
    do
        read ejabberd.log and seek to the first error
        scratch head
        fix any obvious errors
        re-structure as required
    done
    
    git rebase --interactive # squash everything into an easily reviewable blob

--- src/ejabberd_suspend.erl
+++ src/ejabberd_suspend.erl
@@ -27,11 +27,27 @@
 -module(ejabberd_suspend).
 -author('alexey@process-one.net').
 
+-define(MAX_WAIT, 3600). % max num of secs to keep a request on hold
+-define(MAX_INACTIVITY, 30000). % msecs to wait before terminating
+                                % idle sessions
+
+-define(GEN_FSM, p1_fsm).
+
+-behaviour(?GEN_FSM).
+
 %% API
 -export([start/2,
+         start_link/2,
          sleep/3,
          wake/3,
          flush/3,
+         handle_sync_event/4,
+         code_change/4,
+         handle_event/3,
+         handle_info/3,
+         init/1,
+         terminate/3,
+         print_state/1,
 	 reset_stream/1,
 	 send/2,
 	 send_xml/2,
@@ -43,7 +59,32 @@
 
 -include("ejabberd.hrl").
 
--record(state, {sockmod, socket}).
+-record(suspend_state, {
+        sockmod,
+        socket,
+        socket_monitor,
+        fsmref,
+        timer,
+        wait_timer,
+        pause,
+        max_inactivity,
+        output = []}).
+
+% This is what's passed back to ejabberd_c2s as the opaque "socket" datastructure
+% There is redundancy here, but it simplifies the wrapping/unwrapping process,
+% and reduces the amount of bullshit data that gets send to proceses' mailboxes.
+-record(sockdata, {
+        sockmod,
+        socket,
+        fsmref}).
+
+%-define(DBGFSM, true).
+
+-ifdef(DBGFSM).
+-define(FSMOPTS, [{debug, [trace]}]).
+-else.
+-define(FSMOPTS, []).
+-endif.
 
 %%====================================================================
 %% API
@@ -53,75 +94,53 @@
 %% Description:
 %%--------------------------------------------------------------------
 
-start(SockMod, Socket) ->
-    SocketData = #state{sockmod = SockMod,
-        socket = Socket}.
-
-sleep(ejabberd_suspend, SocketData, Data) ->
-    {already_asleep, ejabberd_suspend, SocketData};
+sleep(ejabberd_suspend, SockData, _Data) ->
+    {already_asleep, ejabberd_suspend, SockData};
 sleep(SockMod, Socket, Data) ->
-    SocketData = start(SockMod, Socket),
-    %% Send the requested data out over the socket before we suspend it.
-    (SockMod):send_xml(Socket, Data),
-    {ok, ejabberd_suspend, SocketData}.
+    {ok, Pid} = start_link([Socket, SockMod], []),
+    (SockMod):send(Socket, Data),
+    {ok, ejabberd_suspend, #sockdata{sockmod = SockMod, socket = Socket, fsmref = Pid}}.
 
-wake(ejabberd_suspend, SocketData, Data) ->
-    #state{sockmod = SockMod, socket = Socket} = SocketData,
-    flush_socket(SocketData),
+wake(ejabberd_suspend, SockData, Data) ->
+    gen_fsm:sync_send_all_state_event(SockData#sockdata.fsmref, {wake}),
     %% Take ourself out of the equation (note that we don't actually intercept
     %% any c2s traffic: only s2c traffic, so we just need to shut ourselves down
     %% and return the original sockmod and socket that we were passed.
+    #sockdata{sockmod = SockMod, socket = Socket} = SockData,
     (SockMod):send(Socket, Data),
     {ok, SockMod, Socket};
 wake(SockMod, Socket, _) ->
     {already_awake, SockMod, Socket}.
 
-flush(ejabberd_suspend, SocketData, Data) ->
-    #state{sockmod = SockMod, socket = Socket} = SocketData,
-    flush_socket(SocketData),
+flush(ejabberd_suspend, SockData, Data) ->
+    #sockdata{fsmref = FsmRef, sockmod = SockMod, socket = Socket} = SockData,
+    gen_fsm:sync_send_all_state_event(FsmRef, {flush_socket}),
     (SockMod):send(Socket, Data),
-    {ok, ejabberd_suspend, SocketData}.
+    {ok, ejabberd_suspend, FsmRef}.
 
-flush_socket(SocketData) ->
-    ok.
+send(#sockdata{fsmref = FsmRef}, Packet) ->
+    gen_fsm:sync_send_all_state_event(FsmRef, {send, Packet}).
 
-%% sockmod=gen_tcp|tls|ejabberd_zlib
-send(SocketData, Data) ->
-    case catch (SocketData#state.sockmod):send(
-	     SocketData#state.socket, Data) of
-        ok -> ok;
-	{error, timeout} ->
-	    ?INFO_MSG("Timeout on ~p:send",[SocketData#state.sockmod]),
-	    exit(normal);
-        Error ->
-	    ?DEBUG("Error in ~p:send: ~p",[SocketData#state.sockmod, Error]),
-	    exit(normal)
-    end.
-
-%% Can only be called when in c2s StateData#state.xml_socket is true
-%% This function is used for HTTP bind
-%% sockmod=ejabberd_http_poll|ejabberd_http_bind or any custom module
-send_xml(SocketData, Data) ->
-    catch (SocketData#state.sockmod):send_xml(
-	    SocketData#state.socket, Data).
+send_xml(#sockdata{fsmref = FsmRef}, Packet) ->
+    gen_fsm:sync_send_all_state_event(FsmRef, {send_xml, Packet}).
 
 %% Blindly proxy these calls down the chain.
 reset_stream(SocketData) ->
-    (SocketData#state.sockmod):reset_stream(SocketData#state.socket).
+    (SocketData#sockdata.sockmod):reset_stream(SocketData#sockdata.socket).
 
 change_shaper(SocketData, Shaper) ->
-    (SocketData#state.sockmod):change_shaper(SocketData#state.socket, Shaper).
+    (SocketData#sockdata.sockmod):change_shaper(SocketData#sockdata.socket, Shaper).
 
 monitor(SocketData) ->
-    (SocketData#state.sockmod):monitor(SocketData#state.socket).
+    (SocketData#sockdata.sockmod):monitor(SocketData#sockdata.socket).
 
 get_sockmod(SocketData) ->
-    SocketData#state.sockmod.
+    SocketData#sockdata.sockmod.
 
 close(SocketData) ->
-    (SocketData#state.sockmod):close(SocketData#state.socket).
+    (SocketData#sockdata.sockmod):close(SocketData#sockdata.socket).
 
-sockname(#state{sockmod = SockMod, socket = Socket}) ->
+sockname(#sockdata{sockmod = SockMod, socket = Socket}) ->
     case SockMod of
 	gen_tcp ->
 	    inet:sockname(Socket);
@@ -129,7 +148,7 @@ sockname(#state{sockmod = SockMod, socket = Socket}) ->
 	    SockMod:sockname(Socket)
     end.
 
-peername(#state{sockmod = SockMod, socket = Socket}) ->
+peername(#sockdata{sockmod = SockMod, socket = Socket}) ->
     case SockMod of
 	gen_tcp ->
 	    inet:peername(Socket);
@@ -137,6 +156,137 @@ peername(#state{sockmod = SockMod, socket = Socket}) ->
 	    SockMod:peername(Socket)
     end.
 
+
+start([Socket, SockMod], Opts) ->
+    ?GEN_FSM:start(?MODULE, [Socket, SockMod], fsm_limit_opts(Opts) ++ ?FSMOPTS).
+
+start_link([Socket, SockMod], Opts) ->
+    ?GEN_FSM:start_link(?MODULE, [Socket, SockMod],
+			fsm_limit_opts(Opts) ++ ?FSMOPTS).
+
+
+%%%----------------------------------------------------------------------
+%%% Callback functions from gen_fsm
+%%%----------------------------------------------------------------------
+
+%%----------------------------------------------------------------------
+%% Func: init/1
+%% Returns: {ok, StateName, StateData}          |
+%%          {ok, StateName, StateData, Timeout} |
+%%          ignore                              |
+%%          {stop, StopReason}
+%%----------------------------------------------------------------------
+init([Socket, SockMod]) ->
+    Timer = erlang:start_timer(?MAX_INACTIVITY, self(), []),
+    SocketMonitor = SockMod:monitor(Socket),
+    {ok, sleeping,
+        #suspend_state{ socket = Socket, sockmod = SockMod,
+            socket_monitor = SocketMonitor, timer = Timer},
+        ?MAX_INACTIVITY}.
+
+%%----------------------------------------------------------------------
+%% Func: handle_event/3
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%%----------------------------------------------------------------------
+handle_event(_Event, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+handle_sync_event({send_xml, Packet}, _From, StateName, StateData) ->
+    Output = [Packet | StateData#suspend_state.output],
+    Reply = ok,
+    {reply, Reply, StateName, StateData#suspend_state{output = Output}};
+
+handle_sync_event({send, Packet}, _From, StateName, StateData) ->
+    Output = [Packet | StateData#suspend_state.output],
+    Reply = ok,
+    {reply, Reply, StateName, StateData#suspend_state{output = Output}}.
+
+%%----------------------------------------------------------------------
+%% Func: handle_info/3
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%%----------------------------------------------------------------------
+%% We reached the max_inactivity timeout:
+handle_info({timeout, Timer, _}, _StateName, #suspend_state{timer = Timer} = StateData) ->
+    ?INFO_MSG("Session timeout. Closing suspended session to ~p due to inactivity.", [peername(StateData)]),
+    {stop, normal, StateData};
+
+handle_info({timeout, WaitTimer, _}, StateName,
+	    #suspend_state{wait_timer = WaitTimer} = StateData) ->
+    if true ->
+	    cancel_timer(StateData#suspend_state.timer),
+	    Timer = set_inactivity_timer(StateData#suspend_state.pause,
+					 StateData#suspend_state.max_inactivity),
+	    %%%gen_fsm:reply(StateData#suspend_state.http_receiver, {ok, empty}),
+	    %%%Rid = StateData#suspend_state.rid,
+	    %%%ReqList = [#hbr{rid = Rid,
+			    %%%key = StateData#suspend_state.key,
+			    %%%out = []
+			   %%%} |
+		       %%%[El || El <- StateData#suspend_state.req_list,
+			      %%%El#hbr.rid /= Rid ]
+		      %%%],
+	    {next_state, StateName,
+	     StateData#suspend_state{%%%http_receiver = undefined,
+			     %%%req_list = ReqList,
+			     wait_timer = undefined,
+			     timer = Timer}}
+    end;
+
+handle_info(_, StateName, StateData) ->
+    {next_state, StateName, StateData}.
+
+%%----------------------------------------------------------------------
+%% Func: terminate/3
+%% Purpose: Shutdown the fsm
+%% Returns: any
+%%----------------------------------------------------------------------
+terminate(_Reason, _StateName, _StateData) ->
+    ok.
+
+code_change(_OldVsn, StateName, StateData, _Extra) ->
+    {ok, StateName, StateData}.
+
+print_state(StateData) ->
+    StateData.
+
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+fsm_limit_opts(Opts) ->
+    case lists:keysearch(max_fsm_queue, 1, Opts) of
+	{value, {_, N}} when is_integer(N) ->
+	    [{max_queue, N}];
+	_ ->
+	    case ejabberd_config:get_local_option(max_fsm_queue) of
+		N when is_integer(N) ->
+		    [{max_queue, N}];
+		_ ->
+		    []
+	    end
+    end.
+
+%% Cancel timer and empty message queue.
+cancel_timer(undefined) ->
+    ok;
+cancel_timer(Timer) ->
+    erlang:cancel_timer(Timer),
+    receive
+	{timeout, Timer, _} ->
+	    ok
+    after 0 ->
+	    ok
+    end.
+%%
+%% If client asked for a pause (pause > 0), we apply the pause value
+%% as inactivity timer:
+set_inactivity_timer(Pause, _MaxInactivity) when Pause > 0 ->
+    erlang:start_timer(Pause*1000, self(), []);
+%% Otherwise, we apply the max_inactivity value as inactivity timer:
+set_inactivity_timer(_Pause, MaxInactivity) ->
+    erlang:start_timer(MaxInactivity, self(), []).
+
