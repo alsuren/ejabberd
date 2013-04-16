a4b1b3137aad4e670da5e8cec7c561ebe7532e16
David Laban <alsuren@gmail.com>
2013-04-16 17:16:36 +0100

    Check ejabberd.rosterusers for consistency
    There are 9 allowed states in
    http://tools.ietf.org/html/rfc6121#appendix-A
    
    There are two paths [1] from mutually unsubscribed to mutually subscribed.
    Let's call our example users Fred and George, and the paths A and B.
    
    Path A is:
    1) Fred sends <presence type='subscribe' to='george@YOUR.DOMAIN.HERE'/>
    2) George sends <presence type='subscribe' to='fred@YOUR.DOMAIN.HERE'/>
    3) George sends <presence type='subscribed' to='fred@YOUR.DOMAIN.HERE'/>
    4) Fred sends <presence type='subscribed' to='george@YOUR.DOMAIN.HERE'/>
    
    Path B is:
    1) Fred sends <presence type='subscribe' to='george@YOUR.DOMAIN.HERE'/>
    2b) George sends <presence type='subscribed' to='fred@YOUR.DOMAIN.HERE'/>
    3b) George sends <presence type='subscribe' to='fred@YOUR.DOMAIN.HERE'/>
    4) Fred sends <presence type='subscribed' to='george@YOUR.DOMAIN.HERE'/>
    
    [1] Ignoring paths that involve <presence type='unsubscribe'>,
        and symmetrically-opposite descriptions of the above-described paths.
    
    This select statement asserts that the state our users' rosteritems
    reach after each step is correct and consistent.
    Both users must be on the same domain for this check to be meaningful.
    
    '--' comments at the end of lines correspond to the step that's just been
    completed from the above list. (I wrote the full transition diagram on a
    post-it note, but I think that the above list is approximately as clear.)

---
+++ tools/roster_consistency.sql
@@ -0,0 +1,44 @@
+
+-- Subscriptions within pairs of users on the local server happen in well-defined pairs.
+-- See git commit message for a full explaination
+select
+  ru1.username as us,
+  ru1.jid as them,
+  ru1.subscription as our_subs,
+  ru1.ask as our_ask,
+  ru2.subscription as their_subs,
+  ru2.ask as their_ask
+from
+  ejabberd.rosterusers ru1,
+  ejabberd.rosterusers ru2
+where
+  substring_index(ru1.jid, '@', -1) = 'YOUR.DOMAIN.HERE'
+  and
+  substring_index(ru1.jid, '@', 1) = ru2.username
+  and
+  substring_index(ru2.jid, '@', -1) = 'YOUR.DOMAIN.HERE'
+  and
+  substring_index(ru2.jid, '@', 1) = ru1.username
+  and
+  (
+    (ru1.subscription = 'N' and ru1.ask = 'N' and (ru2.subscription <> 'N' or ru2.ask <> 'N')) -- 0
+    or
+    (ru1.subscription = 'N' and ru1.ask = 'I' and (ru2.subscription <> 'N' or ru2.ask <> 'O')) -- 1
+    or
+    (ru1.subscription = 'N' and ru1.ask = 'O' and (ru2.subscription <> 'N' or ru2.ask <> 'I')) -- 1
+    or
+    (ru1.subscription = 'N' and ru1.ask = 'B' and (ru2.subscription <> 'N' or ru2.ask <> 'B')) -- 2
+    or
+    (ru1.subscription = 'T' and ru1.ask = 'I' and (ru2.subscription <> 'F' or ru2.ask <> 'O')) -- 3
+    or
+    (ru1.subscription = 'F' and ru1.ask = 'O' and (ru2.subscription <> 'T' or ru2.ask <> 'I')) -- 3
+    or
+    (ru1.subscription = 'T' and ru1.ask = 'N' and (ru2.subscription <> 'F' or ru2.ask <> 'N')) -- 2b
+    or
+    (ru1.subscription = 'F' and ru1.ask = 'N' and (ru2.subscription <> 'T' or ru2.ask <> 'N')) -- 2b
+    or
+    (ru1.subscription = 'B' and ru2.subscription <> 'B') -- 4
+  )
+
+limit 10 -- Probably best to run this query on a replica, because it's pretty expensive.
+  ;
